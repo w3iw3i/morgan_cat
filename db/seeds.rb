d29822eec69d938a8f4d41098cdd62f7e352bef3
Shawn Low <chshawn.low@gmail.com>
2021-06-12 15:16:25 +0800

    added scenarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Asset.destroy_all
 
-Asset.create(asset_type: "stocks", amount: 10000, growth_rate: 8, user_id:1, asset_allocation: 30)
-Asset.create(asset_type: "bonds", amount: 2000, growth_rate: 2, user_id:1, asset_allocation: 20)
+Asset.create(asset_type: "cash", amount: 10000, growth_rate: -1.5, user_id:1, asset_allocation: 60)
+Asset.create(asset_type: "stocks", amount: 10000, growth_rate: 8, user_id:1, asset_allocation: 10)
+Asset.create(asset_type: "bonds", amount: 2000, growth_rate: 2, user_id:1, asset_allocation: 10)
 Asset.create(asset_type: "cpfo", amount: 2000, growth_rate: 4, user_id:1, asset_allocation: 15)
 Asset.create(asset_type: "cpfs", amount: 1000, growth_rate: 4, user_id:1, asset_allocation: 3)
 Asset.create(asset_type: "cpfm", amount: 1000, growth_rate: 4, user_id:1, asset_allocation: 2)
