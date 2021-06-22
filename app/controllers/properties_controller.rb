00480d0fcfc3ac1b149fd8c76f0cac636dfd84ac
Chia Wei Ng <chiawei2410@gmail.com>
2021-06-22 22:03:26 +0800

    refactored webscraper services

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 require "nokogiri"
+require_relative "../services/web_scraper.rb"
 
 class PropertiesController < ApplicationController
 
@@ -13,33 +14,11 @@ class PropertiesController < ApplicationController
   def create
     @property = Property.new(property_params)
     @property.user = current_user
-    # @property.property_value = WebScraper.new(@property).get_property_value
-    # @property.lease_remaining = WebScraper.new(@property).get_lease_remaining
-    # raise
+    flat_type = get_flattype
+    webscraper = WebScraper.new(flat_type, @property.floor, @property.unit, @property.area, @property.address)
+    @property.property_value = webscraper.get_property_value
+    @property.lease_remaining = webscraper.get_lease_remaining
 
-    # Get input parameter for webscraping
-    flattype = get_flattype
-    floornum = @property.floor
-    unitnum = @property.unit
-    areasqm = @property.area
-    postalcode = @property.address
-
-    # Get property value from UrbanZoom
-    requesturl = "https://www.urbanzoom.com/api/v1/ml/new_valuation?postal_code=#{postalcode}&floor_number=#{floornum}&unit_number=#{unitnum}&area_in_sqm=#{areasqm}&housing_type=HDB&flat_type=#{flattype}"
-    response = HTTParty.get(requesturl).parsed_response
-    @property.property_value = response["valuation"].to_i
-
-    # Compute lease remaining using completion year from UrbanZoom
-    url = "https://www.urbanzoom.com/house-preview?flatType=#{flattype}&floorNum=#{floornum}&unitNum=#{unitnum}&areaInSqm=#{areasqm}&ref=home-search&housingType=public&postalCode=#{postalcode}"
-    doc = Nokogiri::XML(open(url).read, nil, "utf-8")
-    @year_completed = doc.at_css(".project_details__list").children.children.children[-2].text.to_i
-    @property.lease_remaining = lease_computation
-
-    # browser = Watir::Browser.new :chrome, headless: true
-    # browser.goto(url)
-    # js_doc = browser.element(css: ".zoom-value-prediction").wait_until(&:present?)
-    # value = js_doc.inner_html
-    # browser.close
     if @property.save
       redirect_to properties_path
     end
