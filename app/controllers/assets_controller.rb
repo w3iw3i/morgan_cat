affc81bcfc38cda3b6beae864f00b33ec2e09988
Shawn Low <chshawn.low@gmail.com>
2021-06-26 13:21:11 +0800

    complete cash alloc

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,12 +2,20 @@ class AssetsController < ApplicationController
   def index
     @asset = Asset.new
     @my_asset = Asset.where(user: current_user)
+    update_cash_allocation
+    @current_asset_array = []
+    @my_asset.each do |asset|
+      @current_asset_array << asset.asset_type
+    end
+    @all_asset_types = ["Cash", "Stock", "Bond", "CPF-O", "CPF-M", "CPF-S"]
+    @remaining_asset_types = @all_asset_types - @current_asset_array
   end
 
   def create
     @asset = Asset.new(asset_params)
     @asset.user = current_user
     if @asset.save
+      update_cash_allocation
       redirect_to assets_path
     end
   end
@@ -15,6 +23,7 @@ class AssetsController < ApplicationController
   def destroy
     @asset = Asset.find(params[:id])
     @asset.destroy
+    update_cash_allocation
     redirect_to assets_path
   end
 
@@ -33,6 +42,17 @@ class AssetsController < ApplicationController
 
   def asset_params
     params.require(:asset).permit(:asset_type, :amount, :growth_rate, :asset_allocation)
+  end
+
+  def update_cash_allocation
+    @other_asset_sum = 0
+    @other_assets = Asset.where(user_id: current_user.id).where.not(asset_type: "Cash")
+    @other_assets.each do |other_asset|
+      @other_asset_sum += other_asset.asset_allocation
+    end
+
+    @cash = Asset.where(user_id: current_user.id, asset_type: "Cash")
+    @cash.update(asset_allocation: (100 - @other_asset_sum))
   end
 
 end
